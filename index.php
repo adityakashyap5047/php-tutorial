1ed53a0e39e076bf4ac727450bd04495fb74cb01
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-01-22 22:59:52 +0530

    bootstrap with php

--- index.php
+++ index.php
@@ -3,17 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>php</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
     <?php 
         // This is single line comment in php
-        echo "Hello world"; 
+        echo "Hello world -> "; 
 
         /*
             This is multi line comment and this can be implemented by forward slash with asterisk 
         */
         echo "Welcome, to the world of php";
     ?>
+    <div class="alert alert-success" role="alert">
+        A simple success alert—check it out!
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
