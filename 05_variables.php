c3392fc058dd8a3e234ce0d4b920a423505da176
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-01-23 00:34:34 +0530

    variable in a php

---
+++ 05_variables.php
@@ -0,0 +1,19 @@
+<?php 
+    ECHO "keywords in php is not case sensitive";
+    
+    //Variable -> are containers for storing information
+    // Starts with a $
+    $name = "Aditya";
+    $profile = "Developer";
+
+    // We use <br> tag for line break
+    echo "Heyyy, This is $name and he is a $profile <br><br>";
+
+    // Variables in php are case sensitive
+    $NAME = "Aditya Kumar";
+    echo "Heyy, he is $name and his full name is $NAME <br><br>";
+
+    // php is a loosely typed language
+    $income = 100.5;
+    echo "Heyy, he is $name and his full name is $NAME and his income is $income <br><br>";
+?>
--- index.php
+++ index.php
@@ -17,7 +17,7 @@
         echo "Welcome, to the world of php";
     ?>
     <div class="alert alert-success" role="alert">
-        A simple success alert—check it out!
+        Bootstrap with PHP
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
